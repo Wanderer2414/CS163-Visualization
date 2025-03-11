859988313f58d369f4a439077701f4626f9aba83
Le Van Vi <1782006vile@gmail.com>
2025-03-11 22:10:50 +0700

    Some logic for singlyLinkedList

--- include/SinglyLinkedList.h
+++ include/SinglyLinkedList.h
@@ -1,2 +1,20 @@
 #include <iostream>
 
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class SinglyLinkedList {
+	private:
+		ListNode* head;
+		ListNode* tail;
+	int size;
+	public:
+		void add(int val);
+		void update(int index, int val);
+		void remove(int index);
+		ListNode* search(int val);
+};
+
--- source/SinglyLinkedList.cpp
+++ source/SinglyLinkedList.cpp
@@ -1,2 +1,52 @@
 #include "../include/SinglyLinkedList.h"
 
+void SinglyLinkedList::add(int val)
+{
+	if (!head) {
+		head = new ListNode(val);
+		tail = head;
+	}
+	else {
+		tail->next = new ListNode(val);
+		tail = tail->next;
+	}
+}
+
+void SinglyLinkedList::update(int index, int val)
+{
+	ListNode* temp = head;
+	for (int i = 0; i < index; i++) {
+		if (temp->next) {
+			temp = temp->next;
+		}
+	}
+	temp->val = val;
+}
+
+void SinglyLinkedList::remove(int index)
+{
+	if (index == 1 && head) {
+		ListNode* del = head;
+		head = head->next;
+		delete del;
+	}
+	else {
+		ListNode* temp = head;
+		for (int i = 0; i < index - 1; i++) {
+			temp = temp->next;
+		}
+		ListNode* del = temp->next;
+		temp = del->next;
+		delete del;
+	}
+}
+
+ListNode* SinglyLinkedList::search(int val)
+{
+	ListNode* temp = head;
+	while (temp) {
+		if (temp->val == val) return temp;
+		else temp = temp->next;
+	}
+    return nullptr;
+}
