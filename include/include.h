20acf10c9cae10dc898bc61b6b116ad6e68fbd89
NguyenLeQuocHuy <huynguyenlequoc13@gmail.com>
2025-03-19 21:06:18 +0700

    fix some error

---
+++ include/include.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#define font_link "/home/nguyenlequochuy/projects/cpp_raylib/NodeCollision/CS163-DSAVisualization/asset/Font/PublicSans-Regular.ttf"
+
+#endif
