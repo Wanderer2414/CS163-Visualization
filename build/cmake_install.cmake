040e2b20cecb43aea8566f55e13d3a5346a97bce
BlueAngel05 <hoangdonthienhoa@gmail.com>
2025-04-17 16:29:14 +0700

    cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-# Install script for directory: C:/Github_milu/CS163-DSAVisualization
-=======
 # Install script for directory: C:/Users/Dell/Downloads/CS163-DSAVisualization
->>>>>>> origin/ThienHoa
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/DSAVisual")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/DSAVisualization")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -16,11 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-<<<<<<< HEAD
-    set(CMAKE_INSTALL_CONFIG_NAME "Release")
-=======
-    set(CMAKE_INSTALL_CONFIG_NAME "")
->>>>>>> origin/ThienHoa
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,31 +32,18 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Github_milu/CS163-DSAVisualization/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
+
 if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-<<<<<<< HEAD
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Github_milu/CS163-DSAVisualization/build/${CMAKE_INSTALL_MANIFEST}"
-=======
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 file(WRITE "C:/Users/Dell/Downloads/CS163-DSAVisualization/build/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> origin/ThienHoa
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
